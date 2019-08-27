68fbd7cec8fc06ec66da1bc5da24afb1bff7f766
Mohit Sethi <mohitsethi.32@gmail.com>
2019-08-27 12:34:56 +0530

    Added change preview when anything is changed and added undo/redo.

--- Assets/Scripts/Editor/DynamicCameraEditor.cs
+++ Assets/Scripts/Editor/DynamicCameraEditor.cs
@@ -7,10 +7,13 @@ public class DynamicCameraEditor : Editor
     private float labelWidth = 150f;
     private float buttonWidth = 100f;
     private Feature current;
+    private DynamicCamera main;
 
     public override void OnInspectorGUI()
 	{
-        DynamicCamera main = (DynamicCamera)target;
+        main = (DynamicCamera)target;
+
+        Undo.RecordObject(main, "DynamicCamera");
 
         EditorGUILayout.Space();
 
@@ -28,9 +31,12 @@ public class DynamicCameraEditor : Editor
         main.offset = EditorGUILayout.Vector3Field("Base Target Offset", main.offset);
         main.angle = EditorGUILayout.FloatField("Base Target Angle", main.angle);
 
+        if (GUI.changed)
+            SetPosition();
+
         if (GUILayout.Button("Preview Base", GUILayout.Width(buttonWidth)))
         {
-            main.PositionCamera(true);
+            SetPosition();
         }
 
         EditorGUILayout.Space();
@@ -49,8 +55,11 @@ public class DynamicCameraEditor : Editor
 
             current.toggleFold = EditorGUILayout.Foldout(current.toggleFold, current.id);
 
+            // Preview Button
             if (GUILayout.Button("Preview", GUILayout.Width(buttonWidth)))
-                main.PositionCamera(true, current.id);
+            {
+                SetPosition(current.id);
+            }
 
             // if clicked, remove current from the list
             if (GUILayout.Button("Remove", GUILayout.Width(buttonWidth)))
@@ -64,12 +73,21 @@ public class DynamicCameraEditor : Editor
             // If the class is expanded
             if (current.toggleFold)
             {
+                for (int j = 0; j < main.features.Count; j++)
+                {
+                    if (main.features[j] != current)
+                        main.features[j].toggleFold = false;
+                }
+
                 EditorGUILayout.Space();
 
                 current.id = EditorGUILayout.TextField("ID", current.id);
                 current.target = (Transform) EditorGUILayout.ObjectField("Target", current.target, typeof(Transform), true);
                 current.offset = EditorGUILayout.Vector3Field("Offset", current.offset);
                 current.angle = EditorGUILayout.FloatField("Angle", current.angle);
+
+                if (GUI.changed)
+                    SetPosition(current.id);
 
                 EditorGUILayout.Space();
             }
@@ -84,5 +102,12 @@ public class DynamicCameraEditor : Editor
             Feature temp = new Feature();
             main.features.Add(temp);
         }
+
+        EditorUtility.SetDirty(main);
+    }
+
+    private void SetPosition(string id = "")
+    {
+        main.PositionCamera(true, id);
     }
 }
